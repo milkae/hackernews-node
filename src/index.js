e4492df9b8df742e42c229876f9d8a2549d14e7d
Floriane <floriane@tuta.io>
2019-11-08 17:12:45 -0500

    Basic CRUD for link

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `Hello you`,
-    feed: () => links
+    feed: () => links,
+    link: (_, { id }) => links.find(link => Number(link.id) === Number(id))
   },
   Mutation: {
     post: (_, { description, url }) => {
@@ -23,6 +24,29 @@ const resolvers = {
         url
       };
       links.push(link);
+      return link;
+    },
+    updateLink: (_, { id, url, description }) => {
+      const link = links.find(link => link.id === id);
+      if (!link) {
+        return null;
+      }
+      const updatedLink = {
+        ...link,
+        description: description || link.description,
+        url: url || link.url
+      };
+
+      links = [...links.slice(0, id), updatedLink, ...links.slice(id + 1)];
+
+      return updatedLink;
+    },
+    deleteLink: (_, { id }) => {
+      const link = links.find(link => link.id === id);
+      if (!link) {
+        return null;
+      }
+      links = [...links.slice(0, id), ...links.slice(id + 1)];
       return link;
     }
   }
