0390914ff2ec166a92d6bc2f85433e4b119420e0
Floriane <floriane@tuta.io>
2019-11-09 18:37:09 -0500

    Adds pagination and sorting for feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,9 +1,15 @@
-const feed = async (_, { filter }, { prisma }) => {
+const feed = async (_, { filter, skip, first, orderBy }, { prisma }) => {
   const where = filter
     ? { OR: [{ description_contains: filter }, { url_contains: filter }] }
     : {};
-  const links = await prisma.links({ where });
-  return links;
+  const links = await prisma.links({ where, skip, first, orderBy });
+
+  const count = await prisma
+    .linksConnection({ where })
+    .aggregate()
+    .count();
+
+  return { links, count };
 };
 
 const link = (_, { id }, { prisma }) => prisma.link({ id });
