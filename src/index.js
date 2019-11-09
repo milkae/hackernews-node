d7719ef4c623377ce1af8f8bd56d3f4ab2347644
Floriane <floriane@tuta.io>
2019-11-09 16:42:27 -0500

    Replaces mocked data with prisma

--- src/index.js
+++ src/index.js
@@ -1,60 +1,32 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-let links = [
-  {
-    id: 0,
-    url: "https://graphql.org",
-    description: "GraphQL"
-  }
-];
-
-let idCount = links.length;
+const { prisma } = require("./generated/prisma-client");
 
 const resolvers = {
   Query: {
     info: () => `Hello you`,
-    feed: () => links,
-    link: (_, { id }) => links.find(link => Number(link.id) === Number(id))
+    feed: (_, _1, { prisma }) => prisma.links(),
+    link: (_, { id }, { prisma }) => prisma.link({ id })
   },
   Mutation: {
-    post: (_, { description, url }) => {
-      const link = {
-        id: idCount++,
-        description,
-        url
-      };
-      links.push(link);
-      return link;
+    post: (_, { description, url }, { prisma }) => {
+      return prisma.createLink({ url, description });
     },
-    updateLink: (_, { id, url, description }) => {
-      const link = links.find(link => link.id === id);
-      if (!link) {
-        return null;
-      }
-      const updatedLink = {
-        ...link,
-        description: description || link.description,
-        url: url || link.url
-      };
-
-      links = [...links.slice(0, id), updatedLink, ...links.slice(id + 1)];
-
-      return updatedLink;
+    updateLink: (_, { id, url, description }, { prisma }) => {
+      return prisma.updateLink({
+        data: { url, description },
+        where: { id: id }
+      });
     },
-    deleteLink: (_, { id }) => {
-      const link = links.find(link => link.id === id);
-      if (!link) {
-        return null;
-      }
-      links = [...links.slice(0, id), ...links.slice(id + 1)];
-      return link;
+    deleteLink: (_, { id }, { prisma }) => {
+      return prisma.deleteLink({ id });
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
