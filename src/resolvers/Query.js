5c25b99c23aff2a561ca45f5014e46880428c981
Floriane <floriane@tuta.io>
2019-11-09 18:34:29 -0500

    Allows to filter feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,10 @@
-const feed = (_, _1, { prisma }) => prisma.links();
+const feed = async (_, { filter }, { prisma }) => {
+  const where = filter
+    ? { OR: [{ description_contains: filter }, { url_contains: filter }] }
+    : {};
+  const links = await prisma.links({ where });
+  return links;
+};
 
 const link = (_, { id }, { prisma }) => prisma.link({ id });
 
